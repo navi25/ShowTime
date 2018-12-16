d309a4c9c93f13c1357f6aa718553aa7d7e7cfc0
Navendra Jha <navendra@asu.edu>
2018-12-16 01:34:02 -0700

    Added drawable layouts for home view

---
+++ app/src/androidTest/java/io/navendra/showtime/ExampleInstrumentedTest.kt
@@ -0,0 +1,23 @@
+package io.navendra.showtime
+
+import androidx.test.InstrumentationRegistry
+import androidx.test.runner.AndroidJUnit4
+import junit.framework.TestCase.assertEquals
+import org.junit.Test
+import org.junit.runner.RunWith
+
+
+/**
+ * Instrumented test, which will execute on an Android device.
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+@RunWith(AndroidJUnit4::class)
+class ExampleInstrumentedTest {
+    @Test
+    fun useAppContext() {
+        // Context of the app under test.
+        val appContext = InstrumentationRegistry.getTargetContext()
+        assertEquals("io.navendra.showtime", appContext.packageName)
+    }
+}
---
+++ app/src/main/java/io/navendra/showtime/MainActivity.kt
@@ -0,0 +1,16 @@
+package io.navendra.showtime
+
+import android.os.Bundle
+import android.view.Window
+import android.view.WindowManager
+import androidx.appcompat.app.AppCompatActivity
+
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+    }
+}
